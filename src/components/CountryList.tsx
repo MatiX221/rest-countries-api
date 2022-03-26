751cc90f8fe1e959d60a1afc2e9754e8bfdcb51f
MatiX221 <98061076+MatiX221@users.noreply.github.com>
2022-03-26 21:18:51 +0100

    update

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -3,23 +3,68 @@ import Card from "./Card";
 import Select from "./Select";
 
 function CountryList({ inputValue, filterValue, inputChange, filterChange, countries, getClickedCountry }: any) {
+    const lowerInput = inputValue.toLowerCase();
+    const isShown = (country: any) => {
+        var commonNameList: any = [];
+        var officialNameList: any = [];
+        var translationsCommon: any = [];
+        var translationsOfficial: any = [];
+
+        if (country.name.nativeName != undefined) {
+            Object.keys(country.name.nativeName).forEach((key: any, index: any) => {
+                commonNameList[index] = country.name.nativeName[key].common;
+                officialNameList[index] = country.name.nativeName[key].official;
+            });
+        }
+
+        if (country.translations != undefined) {
+            Object.keys(country.translations).forEach((key: any, index: any) => {
+                translationsCommon[index] = country.translations[key].common;
+                translationsOfficial[index] = country.translations[key].official;
+            });
+        }
+
+        var isNativeName = "";
+        Object.keys(commonNameList).forEach((index: any) => {
+            if (commonNameList[index].toLowerCase().includes(lowerInput)) {
+                isNativeName = "true";
+            }
+        });
+        Object.keys(officialNameList).forEach((index: any) => {
+            if (officialNameList[index].toLowerCase().includes(lowerInput)) {
+                isNativeName = "true";
+            }
+        });
+        Object.keys(translationsCommon).forEach((index: any) => {
+            if (translationsCommon[index].toLowerCase().includes(lowerInput)) {
+                isNativeName = "true";
+            }
+        });
+        Object.keys(translationsOfficial).forEach((index: any) => {
+            if (translationsOfficial[index].toLowerCase().includes(lowerInput)) {
+                isNativeName = "true";
+            }
+        });
+
+        if (
+            country.name.common.toLowerCase().includes(lowerInput) || //
+            country.cca2.toLowerCase().includes(lowerInput) || //
+            country.cca3.toLowerCase().includes(lowerInput) || //
+            isNativeName === "true"
+        ) {
+            return true;
+        }
+
+        return false;
+    };
+
     return (
         <div className="container__grid">
             <Select inputValue={inputValue} inputChange={inputChange} filterChange={filterChange} filterValue={filterValue}></Select>
             <div className="card__grid">
                 {filterValue === "region"
                     ? countries //
-                          .filter(
-                              (country: any) =>
-                                  //   country.name.nativeName != undefined
-                                  //       ? country.name.common.toLowerCase().includes(inputValue.toLowerCase()) || //
-                                  //         country.name.official.toLowerCase().includes(inputValue.toLowerCase()) ||
-                                  //         Object.keys(country.name.nativeName).forEach((key: any) => console.log(country.name.nativeName[key].common.toLowerCase())) //
-                                  //       :
-                                  country.name.common.toLowerCase().includes(inputValue.toLowerCase()) || //
-                                  country.name.official.toLowerCase().includes(inputValue.toLowerCase())
-                              //
-                          )
+                          .filter((country: any) => isShown(country))
                           .map((country: any, index: any) => <Card png={country.flags.png} name={country.name.common} population={country.population} region={country.region} capital={country.capital} getClickedCountry={getClickedCountry} key={index}></Card>)
                     : countries
                           .filter(
